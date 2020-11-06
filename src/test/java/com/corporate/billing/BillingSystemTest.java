df19f3d554e4122669de9e6551b6d1862114d0af
ShirishkumarBari <shirishkumargbari@gmail.com>
2020-11-06 09:53:24 +0530

    add project structure

---
+++ src/test/java/com/corporate/billing/BillingSystemTest.java
@@ -0,0 +1,17 @@
+package com.corporate.billing;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BillingSystemTest {
+    @BeforeEach
+    public void before() {
+
+    }
+
+    @Test
+    public void shouldBeAbleToCalculateBillingAmount(){
+        
+    }
+
+}
